0e1d1efc1a79c8315beaed2d2efa35ad7911e2b3
petrsokerin <petya4343@mail.ru>
2023-01-06 00:48:14 +0300

    add hello.py

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
 
-  sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-  sudo rm -rf /etc/nginx/sites-enabled/default
-  sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
